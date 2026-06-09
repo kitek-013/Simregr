d97424e1fa3f1c4193453cadb1d59e9a8ea6bdb0
kitek-013 <kitewbar@gmail.com>
2026-06-09 11:57:34 +0200

    sums and mallocs

--- src/linregr.c
+++ src/linregr.c
@@ -25,7 +25,7 @@ int sum1(const int *x, size_t n)
 
 double* mnoz1(const double* x,const double* y, size_t n)
 {
-    double* r;
+    double* r = (double*)malloc(n * sizeof(double))
     for(size_t i=0; i<n; i++)
         r[i] = x[i]*y[i];
     return r;
@@ -33,17 +33,30 @@ double* mnoz1(const double* x,const double* y, size_t n)
 
 int* mnoz2(const int* x,const int* y, size_t n)
 {
-    int* r;
+    int* r = (int*)malloc(n * sizeof(int))
     for(size_t i=0; i<n; i++)
         r[i] = x[i]*y[i];
     return r;
 }
 
+double* square1(const double *x, size_t n)
+{
+    double* r = (double*)malloc(n * sizeof(double));
+    for(size_t i=0; i<n; i++)
+        r[i] = x[i] * x[i];
+    return r;
+}
+
+int* square2(const int *x, size_t n)
+{
+    int* r = (int*)malloc(n * sizeof(int));
+    for(size_t i=0; i<n; i++)
+        r[i] = x[i] * x[i];
+    return r;
+}
+
 SEXP lin_regr(SEXP x, SEXP y, SEXP na_rm)
 {
-
-
-
 
     if ((!Rf_isReal(x)) && (!Rf_isInteger(x))) Rf_error("x is not numeric vector");
     if ((!Rf_isReal(y)) && (!Rf_isInteger(y)) Rf_error("y is not numeric vector");
@@ -54,11 +67,16 @@ SEXP lin_regr(SEXP x, SEXP y, SEXP na_rm)
     size_t ny = XLENGTH(y);
     if(!(nx == ny)) Rf_error("x and y are not of the same length");
 
-            double r[2];
+    double r[2];
 
     if(Rf_isInteger(x) && Rf_isInteger(y)){
-        int *px = REAL(x);
-        int *py = REAL(y);
+        int *px = INTEGER(x);
+        int *py = INTEGER(y);
+        int *pxy = mnoz2(x, y, nx);
+        int *pxx = mnoz2(x, x, nx);
+        int *pxsq = square2(x, nx);
+        int pxsum = sum2(px, nx);
+        r[0] = ((nx * sum2(pxy, nx)) - (pxsum * sum2(py, ny)))/(nx * sum2(pxsq, nx) - pxsum * pxsum)
 
     }
     else
