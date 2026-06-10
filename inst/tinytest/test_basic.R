7b9fde2736846e5def4ec25ea6323d48e8f393ac
kitek-013 <kitewbar@gmail.com>
2026-06-10 20:34:30 +0000

    more

--- inst/tinytest/test_basic.R
+++ inst/tinytest/test_basic.R
@@ -3,7 +3,7 @@ library(Simregr)
 
 stopifnot(lin.regr(c(1:10), c(11:20)) == c(1, 10))
 
-stopifnot(round(lin.regr(c(1, 3, 5), c(1, 9, 25)), 2) == c(6, 2.67))
+stopifnot(round(lin.regr(c(1, 3, 5), c(1, 9, 25)), 2) == c(6, -6.33))
 
 
-stopifnot(c(1,2,3,4,5), c(1,2,3,4,10))
+stopifnot(lin.regr(c(1,2,3,4,5), c(1,2,3,4,10)) == c(2, -2))
