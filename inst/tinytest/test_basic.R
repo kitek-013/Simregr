30817310ceb70aa41d54c7dbbd0c006a6bb47c45
kitek-013 <kitewbar@gmail.com>
2026-06-10 20:17:24 +0000

    more

---
+++ inst/tinytest/test_basic.R
@@ -0,0 +1,9 @@
+library(tinytest)
+library(Simregr)
+
+stopifnot(lin.regr(c(1:10), c(11:20)) == c(1, 10))
+
+stopifnot(round(lin.regr(c(1, 3, 5), c(1, 9, 25)), 2) == c(6, 2.67))
+
+
+stopifnot(c(1,2,3,4,5), c(1,2,3,4,10))
