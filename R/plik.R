81d3249de847c48ee2d6e388cf00ddf7f952f196
kitek-013 <kitewbar@gmail.com>
2026-06-10 16:49:01 +0000

    more

--- R/plik.R
+++ R/plik.R
@@ -5,14 +5,19 @@
 #' Linear Regression
 #'
 #' @description
-#' I've no time for this.
+#' Function takes two vectors of the same length and
+#' calculates the best-fit linear function using
+#' Method of Least Squares.
 #'
 #' @param x numeric vector
 #' @param y numeric vector
 #' @param na.rm logical vector of length 1
 #'
 #' @return
-#' Returns a numeric vector.
+#' Returns a numeric vector with two elements,
+#' the first one is slope of the linear function
+#' which is best-fit for the data given, and the second
+#' value is the y-intercept.
 #'
 #' @export
 lin.regr <- function(x, y, na.rm=FALSE)
